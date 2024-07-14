c9a1182f728ac349c3bbde5848048515893e0c22
Pablo Paucar <pabloclb027@gmail.com>
2024-07-14 09:32:08 -0500

    alacritty colros | kitty plantilla | polybar colors | picom espacios i3

---
+++ kitty/templates/hack.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Crear la primera tab y asignarle un nombre
+#kitty @ set-window-title "Arch"
+kitty @ launch --type=tab --title "Scanning"
+# Crear la segunda tab y asignarle un nombre
+kitty @ launch --type=tab --title "Escalation"
